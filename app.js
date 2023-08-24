53d707660c41ad9682f7f37572aa9709b51d9a85
Vincent Ravanel <vravanel@gmail.com>
2023-08-24 18:09:44 +0200

    quete 4 bis

--- app.js
+++ app.js
@@ -16,12 +16,21 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
 
 const userHandlers = require("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
+
+const { validateMovie, validateUser } = require("./validators.js");
+
+  
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+
+app.post("/api/users", validateUser, userHandlers.postUser);
+
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+
 
 app.listen(port, (err) => {
   if (err) {
---
+++ validators.js
@@ -0,0 +1,56 @@
+const database = require("./database");
+const Joi = require("joi");
+
+const movieSchema = Joi.object({
+  title: Joi.string().max(255).required(),
+  director: Joi.string().max(255).required(),
+  year: Joi.string().max(255).required(),
+  color: Joi.string().max(255).required(),
+  duration: Joi.number().required(),
+});
+
+const validateMovie = (req, res, next) => {
+  const { title, director, year, color, duration } = req.body;
+
+  const { error } = movieSchema.validate(
+    { title, director, year, color, duration },
+
+    { abortEarly: false }
+  );
+
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+};
+
+  const userSchema = Joi.object({
+    firstname: Joi.string().max(255).required(),
+    lastname: Joi.string().max(255).required(),
+    email: Joi.string().email().max(255).required(),
+    city: Joi.string().max(255).required(),
+    language: Joi.string().max(255).required(),
+ });
+
+ const validateUser = (req, res, next) => {
+    const { firstname, lastname, email, city, language } = req.body;
+
+    const { error } = userSchema.validate(
+        { firstname, lastname, email, city, language },
+        { abortEarly: false } 
+    );
+
+    if (error) {
+        res.status(422).json({ validationErrors: error.details });
+    } else {
+        next();
+    }
+ }
+
+
+  module.exports = {
+    validateMovie,
+    validateUser,
+  };
+  
